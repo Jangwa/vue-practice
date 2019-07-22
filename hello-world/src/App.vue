6e6402b62d4dd00e5c824aee3098a78ea3ead12f
Amar Kachari <amarkachari@BZLAP068.local>
2019-07-22 16:35:03 +0530

    Vue Hello World

---
+++ hello-world/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg={aTestDataVar} :aList={aList} />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data: function () {
+    return {
+      aTestDataVar: 7,
+      aList: [
+        { text: 1 },
+        { text: 2 },
+        { text: 3 },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello" v-if="msg.aTestDataVar === 7">
+    My First Hellow Word {{ msg.aTestDataVar === 7 ? 'Seven' : 'Not Seven' }}
+    <ol>
+      <li v-for="item in aList.aList" v-bind:key="item.text">
+        {{ item.text }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: Object,
+    aList: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
